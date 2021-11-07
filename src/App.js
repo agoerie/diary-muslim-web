1865a81b42f22df888e09e49485495452375b963
agoerie <proyekagung@gmail.com>
2021-11-07 16:55:26 +0700

    create about page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./pages/landing";
 import TermPage from "./pages/term";
 import PrivacyPage from "./pages/privacy";
+import AboutPage from "./pages/about";
 
 const App = () => (
   <Router>
@@ -11,6 +12,7 @@ const App = () => (
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/term" component={TermPage} />
         <Route exact path="/privacy" component={PrivacyPage} />
+        <Route exact path="/about" component={AboutPage} />
       </Switch>
     </>
   </Router>
---
+++ src/pages/about/index.js
@@ -0,0 +1,41 @@
+import "../term/styles.css";
+
+const aboutPage = () => {
+  return (
+    <div class="about-section">
+      <h1>Tentang Kami</h1>
+      <span class="span-bold">Visi & Misi</span>
+      <p>
+        <b>Kami</b> sebagai pengembang dan penerbit aplikasi DiaryMuslim, dalam
+        mengembangkan aplikasi digital Al-Quran ini, juga berkonsultasi terkait
+        mushaf Al–Quran dengan Forum Pelayan Al-Quran ( Yayasan Pelayan Al-Quran
+        Mulia) yang berlokasi di Vila Inti Persada Blok A3/21A Pamulang Timur,
+        Tangerang Selatan yang dipimpin oleh Bapak H Zarkasi Afif,MA yang juga
+        sebagai Ketua Tim IT Lajnah Pentashihan Mushaf Al-Qur'an (LPMQ) Kemenag
+        RI.
+      </p>
+      <p>
+        <b>Visi</b> Kami dalam mengembangkan Aplikasi ini untuk mengajak dan
+        mengingatkan saudara Muslim untuk selalu menjadikan AlQuran sebagai
+        referensi utama dalam hidup
+      </p>
+      <p>
+        <b>Misi</b> Kami adalah menghubungkan semua saudara Muslim di dunia
+        dengan memberi kemudahan membangun komunitas agar mereka menjadi lebih
+        mudah saling mengingatkan, merutinkan membaca, mendengarkan dan berbagi
+        motivasi dan inspirasi yang bersumber dari Al-Quran
+      </p>
+      <span class="span-bold">Informasi Mushaf Al-Quran</span>
+      <p>
+        Menggunakan Mushaf Standard Indonesia dan Terjemahan Bahasa Indonesia
+        Edisi 2019
+      </p>
+      <span class="span-bold">Kontak Kami</span>
+      <p>
+        Email: <a href="mailto:admin@diarymuslim.com">admin@diarymuslim.com</a>
+      </p>
+    </div>
+  );
+};
+
+export default aboutPage;
