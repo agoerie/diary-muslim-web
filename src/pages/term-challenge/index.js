b169c2ca792c6d87c4e6489f8887f87bb9fe6f91
Agung Wijaya <proyekagung@gmail.com>
2023-03-15 23:02:14 +0700

    revision term challenge content

--- src/pages/term-challenge/index.js
+++ src/pages/term-challenge/index.js
@@ -9,51 +9,45 @@ const termChallengePage = () => {
         ber-istiqomah setiap hari membaca Al- Quran <b>satu hari satu Juz</b>
       </p>
       <span class="span-bold">
-        1. Peserta harus join challenge – Program 1 Day 1 Juz
+        1. Peserta harus join challenge – Program One Day One Juz
       </span>
       <p></p>
       <span class="span-bold">
-        2. Peserta menyelesaikan program khatam baca Al-Quran selama Bulan
-        Ramadhan ( tanggal mulai dan berakhir sesuai ketentuan yg sudah di
-        tetapkan)
+        2. Peserta menyelesaikan program khatam baca Al-Quran ( tanggal mulai
+        dan berakhir sesuai ketentuan yg sudah ditetapkan)
       </span>
       <p></p>
       <span class="span-bold">
-        3. Peserta melakukan tilawah / baca Al-Quran hanya di aplikasi Diary
+        3. Peserta melakukan tilawah / membaca Al-Quran hanya di Aplikasi Diary
         Muslim
       </span>
       <p></p>
       <span class="span-bold">
-        4. Tilawah Al-Quran setiap hari dan berurut dimulai dari hari 1 dari juz
-        1 sampai dengan hari 30 juz 30
+        4. Tilawah Al-Quran dilakukan setiap hari dan berurutan dimulai dari
+        hari 1 ( Day-1) dari juz 1 sampai dengan hari 30 (Day-30) Juz 30.
       </span>
       <p></p>
       <span class="span-bold">
-        5. Peserta yang bisa menyelesaikan sesuai target akan mendapat Trophy
-        dan Poin
+        5. Peserta yang bisa menyelesaikan sesuai target akan mendapat Badge dan
+        Poin
       </span>
       <ul>
         <li>
-          Trophy Emas = untuk peserta yg mencapai target 30 Juz dan sesuai
-          aturan waktu yg ditetapkan yaitu satu hari satu juz
+          Badge Emas = untuk peserta yang selesai membaca 30 Juz dan sesuai
+          aturan waktu yang ditetapkan yaitu satu hari satu juz
         </li>
         <li>
-          Trophy Perak = untuk peserta yg mencapai target 30 Juz tapi tidak
-          sesuai target waktu yg ditentukan , misal : selesai 30 juz tapi hanya
-          dalam 10 hari
+          Badge Perak = untuk peserta yang selesai membaca 30 Juz tapi tidak
+          sesuai target waktu yang ditentukan , misalnya : Membaca Al-Quran 30
+          juz tapi hanya dalam 10 hari
         </li>
         <li>
           Jumlah poin yang didapat akan dihitung berdasarkan kehadiran mengaji
-          per hari-nya, Perhitungan poin seperti berikut :
+          per hari-nya, Perhitungan poin misalnya seperti berikut :
           <div>1 hari = 1 juz = 24 poin</div>
           <div>contoh : 30 hari x 24 = 720 poin ; 10 hari x 24 = 240 poin</div>
         </li>
       </ul>
-      <p></p>
-      <span class="span-bold">
-        6. Mengikuti estimasi waktu per ayat yang ditetapkan sistem contoh:
-        3ayatx2dtk/ayat = 6dtk
-      </span>
     </div>
   );
 };
