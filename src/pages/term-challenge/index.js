50227822ef362f35aa192aa0905f86b974815440
Agung Wijaya <proyekagung@gmail.com>
2023-07-20 16:38:14 +0700

    add term challenge ayat

--- src/pages/term-challenge/index.js
+++ src/pages/term-challenge/index.js
@@ -46,6 +46,49 @@ const termChallengePage = () => {
           <div>contoh : 30 hari x 10 = 300 poin ; 10 hari x 10 = 100 poin</div>
         </li>
       </ul>
+      <br />
+      <h4>Syarat & Ketentuan – Tantangan Harian Per Ayat</h4>
+      <p class="line-space">
+        Program ini bertujuan untuk menjaga kita selalu berinteraksi dan
+        Istiqomah setiap hari membaca Al-Quran <b>Satu Hari Satu Ayat</b>
+      </p>
+      <span>1. Peserta harus join challenge – Program One Day One Verse</span>
+      <p></p>
+      <span>
+        2. Peserta menyelesaikan program khatam baca Al-Quran ( tanggal mulai
+        dan berakhir sesuai ketentuan yg sudah ditetapkan)
+      </span>
+      <p></p>
+      <span>
+        3. Peserta melakukan tilawah / membaca Al-Quran hanya di Aplikasi Diary
+        Muslim
+      </span>
+      <p></p>
+      <span>
+        4. Tilawah Al-Quran dilakukan setiap hari dan berurutan dimulai dari
+        hari-1 ( Day-1).
+      </span>
+      <p></p>
+      <span>
+        5. Peserta yang bisa menyelesaikan sesuai target akan mendapat Badge dan
+        Poin
+      </span>
+      <ul>
+        <li>
+          Badge Emas = untuk peserta yang selesai membaca full 100% sesuai
+          periode tanpa absen
+        </li>
+        <li>
+          Badge Perak = untuk peserta yang selesai membaca 50% target dalam
+          periode challenge
+        </li>
+        <li>
+          Jumlah poin yang didapat akan dihitung berdasarkan kehadiran mengaji
+          per hari-nya, Perhitungan poin misalnya seperti berikut :
+          <div>1 hari = 1 ayat = 1 poin</div>
+          <div>contoh : 30 hari x 1 = 30 poin ; 10 hari x 1 = 10 poin</div>
+        </li>
+      </ul>
     </div>
   );
 };
