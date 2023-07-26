8d5198d62e3a6fa92afaec8cd59ddd37db1eb6f5
Agung Wijaya <proyekagung@gmail.com>
2023-07-26 23:00:36 +0700

    separate term page between juz & ayat

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import TermPage from "./pages/term";
 import PrivacyPage from "./pages/privacy";
 import AboutPage from "./pages/about";
 import TermChallengePage from "./pages/term-challenge";
+import TermChallengeAyatPage from "./pages/term-challenge-ayat";
+import TermChallengeJuzPage from "./pages/term-challenge-juz";
 
 const App = () => (
   <Router>
@@ -15,6 +17,16 @@ const App = () => (
         <Route exact path="/privacy" component={PrivacyPage} />
         <Route exact path="/about" component={AboutPage} />
         <Route exact path="/termchallenge" component={TermChallengePage} />
+        <Route
+          exact
+          path="/term-challenge-juz"
+          component={TermChallengeJuzPage}
+        />
+        <Route
+          exact
+          path="/term-challenge-ayat"
+          component={TermChallengeAyatPage}
+        />
       </Switch>
     </>
   </Router>
